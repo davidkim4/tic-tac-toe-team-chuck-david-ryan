1e6cbd40ca9405abb90c528a184a6399eb47b269
davidkim4 <dkim4016@gmail.com>
2020-09-17 13:17:38 -0700

    finished all bullet points, might refactor the reset after game won

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
   }
 
   restartGame = () => {
-    let { squares, turns} = this.state
+    let { squares, turns } = this.state
     turns = 0
-    this.setState({turns: turns})
+    this.setState({ turns: turns })
     squares = [...Array(9).fill("")]
-    this.setState({squares: squares})
+    this.setState({ squares: squares })
   }
 
   handleLocation = (indexLocation) => {
@@ -35,51 +35,53 @@ class App extends Component {
       else {
         squares[indexLocation] = "o"
       }
-      this.setState({squares: squares})
+      this.setState({ squares: squares })
       turns += 1
-      this.setState({turns: turns})
+      this.setState({ turns: turns })
       console.log(squares)
     }
   }
 
 
-   checkWinner = () => {
+  checkWinner = () => {
     let { squares } = this.state
 
     const winningCombo =
-    [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-    for (let i = 0; i < winningCombo.length; i++){
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
+    for (let i = 0; i < winningCombo.length; i++) {
       const [a, b, c] = winningCombo[i]
-      console.log(a,b,c)
-      console.log(squares)
+      //console.log(a, b, c)
+      //console.log(squares)
       let winner = squares[a]
-      console.log(winner)
+      //console.log(winner)
       if ((squares[a] === 'x' && squares[b] === 'x' && squares[c] === 'x') ||
-    (squares[a] === 'o' && squares[b] === 'o' && squares[c] === 'o')){
+        (squares[a] === 'o' && squares[b] === 'o' && squares[c] === 'o')) {
         // if(squares[a] === squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-          console.log("were here")
-          return winner
+        console.log("were here")
+        return winner
       }
     }
   }
 
   gameResult = (winner) => {
-    let{squares, turns} = this.state
-      if (winner === "x" || winner === "o"){
-        alert(winner.toUpperCase() + ' is the winner!!!')
-      }
-      else if(turns === squares.length){
-        alert( "Cats Game")
-      }
+    let { squares, turns } = this.state
+    //console.log(turns)
+    if (winner === "x" || winner === "o") {
+      alert(winner.toUpperCase() + ' is the winner!!!')
+      this.restartGame()
+    }
+    else if (turns === squares.length - 1) {
+      alert("Cats Game")
+    }
 
   }
 
@@ -107,7 +109,7 @@ class App extends Component {
         <Message
           gameResult={this.gameResult}
         />
-        <button onClick = {this.restartGame}>Restart Game</button>
+        <button onClick={this.restartGame}>Restart Game</button>
       </React.Fragment>
     )
   }
